0289bd2af9949ad2f1260d035874912b2fa2b0ae
Alexandra Lopezgurria <a.lopezgurria@gmail.com>
2023-11-27 17:56:52 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Alexandra LOPEZ-GURRIA on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
